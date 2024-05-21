3f2de7dd5a6b32a6efff2392bff62287196edbca
enrico-amerio <amerio.enrico95@gmail.com>
2024-05-21 17:13:36 +0200

    [add]

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.guest')
+@section('content')
+<h1>Home Dashboard</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController;
+use App\Http\Controllers\Admin\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,14 +15,21 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// Rotte pubbliche
+Route::get('/', [PageController::class, 'index'])->name('home');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Rotte admin
+Route::middleware('auth','verified')
+        ->prefix('admin')
+        ->name('admin.')
+        ->group(function(){
+            Route::get('/',[DashboardController::class, 'index'])->name('home');
+        });
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+//Rotte Auth
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
